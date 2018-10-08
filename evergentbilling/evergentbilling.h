b45edca6378e5dcc9dde800642f6ffdd3336f940
Ryan Hasan <ryan.hasan@gmail.com>
2018-10-08 11:42:26 -0700

    Initial Commit

---
+++ evergentbilling/evergentbilling.h
@@ -0,0 +1,19 @@
+//
+//  evergentbilling.h
+//  evergentbilling
+//
+//  Created by Aminul Hasan on 10/8/18.
+//  Copyright © 2018 Evergent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for evergentbilling.
+FOUNDATION_EXPORT double evergentbillingVersionNumber;
+
+//! Project version string for evergentbilling.
+FOUNDATION_EXPORT const unsigned char evergentbillingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <evergentbilling/PublicHeader.h>
+
+
